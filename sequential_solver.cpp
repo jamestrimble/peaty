62d582c0a57eb27f54bb8afaa1ec587a8ab4b977
James Trimble <james.trimble@yahoo.co.uk>
2019-03-23 22:03:51 +0000

    Interleave local search and complete search

--- sequential_solver.cpp
+++ sequential_solver.cpp
@@ -13,84 +13,6 @@
 #include <random>
 #include <vector>
 
-class MWC {
-    Graph & g;
-    const Params params;
-    Colourer & colourer;
-    VtxList & incumbent;
-    vector<vector<unsigned long long>> branch_vv_bitsets;
-    vector<vector<unsigned long long>> new_P_bitsets;
-    const vector<int> & vertex_numbers_in_original_graph;
-
-    auto update_incumbent_if_necessary(VtxList & C)
-    {
-        if (C.total_wt > incumbent.total_wt) {
-            incumbent = C;
-
-            for (unsigned i=0; i<incumbent.vv.size(); i++)
-                incumbent.vv[i] = vertex_numbers_in_original_graph[incumbent.vv[i]];
-
-            std::cout << "c TMP " << incumbent.total_wt << std::endl;
-        }
-    }
-
-    void expand(VtxList& C, vector<unsigned long long> & P_bitset, long & search_node_count,
-            std::atomic_int & ind_set_upper_bound)
-    {
-        ++search_node_count;
-
-        if (bitset_empty(P_bitset, g.numwords)) {
-            update_incumbent_if_necessary(C);
-            return;
-        }
-
-        int ind_set_upper_bound_int = ind_set_upper_bound.load();
-        if (ind_set_upper_bound_int != -1 && int(incumbent.vv.size()) == ind_set_upper_bound_int) {
-            return;
-        }
-
-        vector<unsigned long long> & branch_vv_bitset = branch_vv_bitsets[C.vv.size()];
-        if (branch_vv_bitset.empty())
-            branch_vv_bitset.resize(g.numwords);
-        else
-            std::fill(branch_vv_bitset.begin(), branch_vv_bitset.end(), 0);
-
-        long target = incumbent.total_wt - C.total_wt;
-        if (colourer.colouring_bound(P_bitset, branch_vv_bitset, target)) {
-            vector<unsigned long long> & new_P_bitset = new_P_bitsets[C.vv.size()];
-            if (new_P_bitset.empty())
-                new_P_bitset.resize(g.numwords);
-
-            bitset_intersect_with_complement(P_bitset, branch_vv_bitset, g.numwords);
-
-            int v;
-            while ((v=first_set_bit(branch_vv_bitset, g.numwords))!=-1) {
-                unset_bit(branch_vv_bitset, v);
-                bitset_intersection_with_complement(P_bitset, g.bit_complement_nd[v], new_P_bitset, g.numwords);
-                C.push_vtx(v, g);
-                expand(C, new_P_bitset, search_node_count, ind_set_upper_bound);
-                set_bit(P_bitset, v);
-                C.pop_vtx(g);
-            }
-        }
-    }
-
-public:
-    MWC(Graph & g, const Params params, VtxList & incumbent, Colourer & colourer,
-            const vector<int> & vertex_numbers_in_original_graph)
-            : g(g), params(params), colourer(colourer), incumbent(incumbent), branch_vv_bitsets(g.n), new_P_bitsets(g.n),
-              vertex_numbers_in_original_graph(vertex_numbers_in_original_graph)
-    {
-    }
-
-    auto run(VtxList & C, long & search_node_count, std::atomic_int & ind_set_upper_bound) -> void
-    {
-        vector<unsigned long long> P_bitset(g.numwords, 0);
-        set_first_n_bits(P_bitset, g.n);
-        expand(C, P_bitset, search_node_count, ind_set_upper_bound);
-    }
-};
-
 class FastSet
 {
     vector<bool> in_set;
@@ -139,7 +61,8 @@ class LocalSearcher
     vector<bool> ind_set;
     int ind_set_size;
     int tabu_duration;
-    int time;
+    unsigned long long time;
+    unsigned long long local_time_limit = 5000;
     vector<int> last_time_changed;
     VtxList & incumbent;
     std::mt19937 mt19937;
@@ -153,6 +76,11 @@ public:
             set_of_vv_with_no_conflicts.add(i);
             set_of_vv_with_one_conflict.remove(i);
         }
+    }
+
+    unsigned long long get_time()
+    {
+        return time;
     }
 
     void reset()
@@ -226,7 +154,7 @@ public:
                     incumbent.push_vtx(i, 1);
                 }
             }
-            std::cout << "incumbent " << incumbent.vv.size() << std::endl;
+            std::cout << "c incumbent from local search " << incumbent.vv.size() << std::endl;
         }
     }
 
@@ -267,33 +195,108 @@ public:
         }
     }
 
-    int find_lower_bound()
+    void search()
     {
-        int local_time_limit = 5000;
-        int iter_num = 0;
-        while (time < 1000000) {
-            int local_time = 0;
-            int local_best = 0;
-            while (local_time < local_time_limit) {
-                greedily_add_to_is();
-                do_swap_or_deletion();
-                do_swap_or_deletion();
-                if (ind_set_size > local_best) {
-                    local_best = ind_set_size;
-                    local_time = 0;
-                }
-                ++local_time;
-                ++time;
-                if (0 == (time % 100000)) {
-                    std::cout << "c time " << time << std::endl;
-                }
+        unsigned long long local_time = 0;
+        int local_best = 0;
+        while (local_time < local_time_limit) {
+            greedily_add_to_is();
+            do_swap_or_deletion();
+            do_swap_or_deletion();
+            if (ind_set_size > local_best) {
+                local_best = ind_set_size;
+                local_time = 0;
             }
-            local_time_limit = local_time_limit + local_time_limit / 1000;
-            reset();
-            ++iter_num;
+            ++local_time;
+            ++time;
+            if (0 == (time % 100000)) {
+                std::cout << "c time " << time << std::endl;
+            }
+        }
+        local_time_limit = local_time_limit + local_time_limit / 1000;
+        reset();
+    }
+};
+
+class MWC {
+    Graph & g;
+    const Params params;
+    Colourer & colourer;
+    VtxList & incumbent;
+    vector<vector<unsigned long long>> branch_vv_bitsets;
+    vector<vector<unsigned long long>> new_P_bitsets;
+    const vector<int> & vertex_numbers_in_original_graph;
+    LocalSearcher & local_searcher;
+
+    auto update_incumbent_if_necessary(VtxList & C)
+    {
+        if (C.total_wt > incumbent.total_wt) {
+            incumbent = C;
+
+            for (unsigned i=0; i<incumbent.vv.size(); i++)
+                incumbent.vv[i] = vertex_numbers_in_original_graph[incumbent.vv[i]];
+
+            std::cout << "c TMP " << incumbent.total_wt << std::endl;
+        }
+    }
+
+    void expand(VtxList& C, vector<unsigned long long> & P_bitset, long & search_node_count,
+            std::atomic_int & ind_set_upper_bound)
+    {
+        ++search_node_count;
+        if (search_node_count > local_searcher.get_time()) {
+            local_searcher.search();
         }
 
-        return incumbent.vv.size();
+        if (bitset_empty(P_bitset, g.numwords)) {
+            update_incumbent_if_necessary(C);
+            return;
+        }
+
+        int ind_set_upper_bound_int = ind_set_upper_bound.load();
+        if (ind_set_upper_bound_int != -1 && int(incumbent.vv.size()) == ind_set_upper_bound_int) {
+            return;
+        }
+
+        vector<unsigned long long> & branch_vv_bitset = branch_vv_bitsets[C.vv.size()];
+        if (branch_vv_bitset.empty())
+            branch_vv_bitset.resize(g.numwords);
+        else
+            std::fill(branch_vv_bitset.begin(), branch_vv_bitset.end(), 0);
+
+        long target = incumbent.total_wt - C.total_wt;
+        if (colourer.colouring_bound(P_bitset, branch_vv_bitset, target)) {
+            vector<unsigned long long> & new_P_bitset = new_P_bitsets[C.vv.size()];
+            if (new_P_bitset.empty())
+                new_P_bitset.resize(g.numwords);
+
+            bitset_intersect_with_complement(P_bitset, branch_vv_bitset, g.numwords);
+
+            int v;
+            while ((v=first_set_bit(branch_vv_bitset, g.numwords))!=-1) {
+                unset_bit(branch_vv_bitset, v);
+                bitset_intersection_with_complement(P_bitset, g.bit_complement_nd[v], new_P_bitset, g.numwords);
+                C.push_vtx(v, g);
+                expand(C, new_P_bitset, search_node_count, ind_set_upper_bound);
+                set_bit(P_bitset, v);
+                C.pop_vtx(g);
+            }
+        }
+    }
+
+public:
+    MWC(Graph & g, const Params params, VtxList & incumbent, Colourer & colourer,
+            const vector<int> & vertex_numbers_in_original_graph, LocalSearcher & local_searcher)
+            : g(g), params(params), colourer(colourer), incumbent(incumbent), branch_vv_bitsets(g.n), new_P_bitsets(g.n),
+              vertex_numbers_in_original_graph(vertex_numbers_in_original_graph), local_searcher(local_searcher)
+    {
+    }
+
+    auto run(VtxList & C, long & search_node_count, std::atomic_int & ind_set_upper_bound) -> void
+    {
+        vector<unsigned long long> P_bitset(g.numwords, 0);
+        set_first_n_bits(P_bitset, g.n);
+        expand(C, P_bitset, search_node_count, ind_set_upper_bound);
     }
 };
 
@@ -303,8 +306,8 @@ auto sequential_mwc(const SparseGraph & g, const Params params, VtxList & incumb
     VtxList C(g.n);
 
     LocalSearcher ls(g, incumbent);
-    int lower_bound = ls.find_lower_bound();
-    std::cout << "Lower bound " << lower_bound << std::endl;
+    for (int i=0; i<10; i++)
+        ls.search();
 
     auto vv0 = initialise(g);
 //    printf("Initial incumbent weight %ld\n", incumbent.total_wt);
@@ -318,6 +321,6 @@ auto sequential_mwc(const SparseGraph & g, const Params params, VtxList & incumb
     Graph ordered_subgraph = ordered_graph.complement_of_induced_subgraph(vv1);
     std::shared_ptr<Colourer> colourer = Colourer::create_colourer(ordered_subgraph, params);
 
-    MWC(ordered_subgraph, params, incumbent, *colourer, vv0).run(
+    MWC(ordered_subgraph, params, incumbent, *colourer, vv0, ls).run(
             C, search_node_count, ind_set_upper_bound);
 }
